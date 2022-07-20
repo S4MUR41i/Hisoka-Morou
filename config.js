a19d38423939f82105e839a4dc1760c5afe30a9d
xganss <79078097+S4MUR41i@users.noreply.github.com>
2022-07-21 05:13:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6282136432456']
+global.premium = ['6282136432456']
+global.packname = 'Putra'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
